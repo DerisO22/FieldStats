01c0e907faf768d071bdc1dd46d2c33db80ece55
deris.o2024 <deris.omalley@mymail.champlain.edu>
2025-05-05 12:01:32 -0400

    postgres configured, .env setup / gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import pg from 'pg';
 import cors from 'cors';
+import dotenv from 'dotenv'
+dotenv.config();
 
 const app = express();
 const port = 3001;
-
 
 app.use(cors({
     origin: 'http://localhost:5173',
@@ -12,19 +14,35 @@ app.use(cors({
 
 app.use(express.json());
 
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-
-    console.log('Login request received:', req.body);
+async function setupApp() {
+    const pgClient = new pg.Client({
+        database: process.env.DATABASE_DATABASE,
+        password: process.env.DATABASE_PASSWORD,
+        user: process.env.DATABASE_USERNAME
+    });
     
-    console.log(`Username: ${username}, Password: ${password}`);
-    if ( username === "ADMIN" && password === "ADMIN") {
-        res.json({ success: true });
-    } else {
-        res.status(401).json({ success: false });
-    }
-})
+    await pgClient.connect();
 
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Server running on port ${port}`);
-})
+    app.get('/data', (req, res) => {
+        pgClient.query('SELECT * FROM posts;')
+    })
+
+    app.post('/login', (req, res) => {
+        const { username, password } = req.body;
+    
+        console.log('Login request received:', req.body);
+        
+        console.log(`Username: ${username}, Password: ${password}`);
+        if ( username === "ADMIN" && password === "ADMIN") {
+            res.json({ success: true });
+        } else { 
+            res.status(401).json({ success: false });
+        }
+    })
+    
+    app.listen(port, '0.0.0.0', () => {
+        console.log(`Server running on port ${port}`);
+    })
+}
+
+setupApp();
