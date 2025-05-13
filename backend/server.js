d85b558adc72d8cb9d90eb85cf7d87ee89e8e3ab
deris.o2024 <deris.omalley@mymail.champlain.edu>
2025-05-12 20:14:00 -0400

    Set up enpoints and frontend display for news (Need to design the cards)

--- backend/server.js
+++ backend/server.js
@@ -202,6 +202,39 @@ async function setupApp() {
         }
     })
 
+    /**
+     * News Related API Endpoints
+     */
+    // 'All' news data
+    app.get('/news_data', async (req, res) => {
+        try {
+            const query = `SELECT * FROM news 
+                       LIMIT 100;`
+            const result = await pgClient.query(query);
+
+            console.log(result.rows);
+            res.json(result.rows);
+        } catch (error) {
+            console.error('Error fetching news data: ', error);
+            res.status(500).json({ error: error.message });
+        }
+    })
+
+    // featured news data
+    app.get('/news_data/featured', async (req, res) => {
+        try {
+            const query = `SELECT * FROM news
+                           WHERE featured = TRUE
+                           LIMIT 20;`
+            
+            const result = await pgClient.query(query);
+            res.json(result.rows);
+        } catch (error) {
+            console.error('Error fetching featured news data: ', error);
+            res.status(500).json({ error: error.message });
+        }
+    })
+
     // Protected API Endpoints (Editing/Deleting/Adding):
     
 
