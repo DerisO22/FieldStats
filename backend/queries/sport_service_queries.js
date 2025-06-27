8ca4cf399ec74217c2e35b5a4ab5180fa78e705e
deris.o2024 <deris.omalley@mymail.champlain.edu>
2025-06-27 14:29:24 -0400

    forgot to save query for sport genders

--- backend/queries/sport_service_queries.js
+++ backend/queries/sport_service_queries.js
@@ -11,7 +11,9 @@ const SportQuerys = {
     CREATE: `INSERT INTO sports (sport_name, sport_description, has_gender_divisions) 
              VALUES ($1, $2, $3);`,
 
-    RETRIEVE_SPORT_GENDERS: `SELECT * FROM sport_genders`,
+    RETRIEVE_SPORT_GENDERS: `SELECT sg.sport_id, sg.gender_id, g.gender 
+                             FROM sport_genders sg
+                             JOIN genders g ON sg.gender_id = g.gender_id`,
 
 	// Probably in the detail pages of sports
     RETRIEVE_NEWS_BY_SPORT: `SELECT * 
